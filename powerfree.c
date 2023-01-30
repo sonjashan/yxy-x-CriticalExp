a83ed17947ba31a90bf273d865f3d536a2fcd3f3
sonjashan <102500611+sonjashan@users.noreply.github.com>
2023-01-30 16:44:09 -0500

    finished testing for npPowerfree()

--- powerfree.c
+++ powerfree.c
@@ -11,21 +11,18 @@
 int npPowerfree(int str[], int sLen, int n, int p){
     // for each letter in str
     for(int i=0; i <= sLen - n; i++){
-        int haul = 0;
+        int halt = 0;
         // for each letter in period
         for(int j=0; j < p; j++){
             for(int step = p; j + step < n; step += p){
                 if(str[i + j] != str[i + j + step]){
-                    printf("i = %d\n", i);
-                    printf("j = %d\n", j);
-                    printf("step = %d\n", step);
-                    haul = 1;
+                    halt = 1;
                     break;
                 }
             }
-            if(haul) break;
+            if(halt) break;
         }
-        if(!haul) return 0;
+        if(!halt) return 0;
     }
     return 1;
 }
@@ -35,6 +32,11 @@ int main(){
     int s[] = {2, 1, 2, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3};
     int sLen = 16;
     int n = 13; 
-    int p = 5;
-    printf("is the string np powerfree? %d\n", npPowerfree(s, sLen, n, p));
+    int p = 6;
+    printf("is the string %d/%d-powerfree? %d\n", n, p, npPowerfree(s, sLen, n, p));
 }
+
+
+// printf("i = %d\n", i);
+// printf("j = %d\n", j);
+// printf("step = %d\n", step);
