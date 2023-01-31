6ea388b053d4f78690f9fbfa515a69cecfa0acd3
sonjashan <102500611+sonjashan@users.noreply.github.com>
2023-01-30 20:40:51 -0500

    added function to apply an morphism

--- powerfree.c
+++ powerfree.c
@@ -7,7 +7,7 @@
 // assume finite length
 // n is length of factor, p is length of period
 // testing n/p+ power is essentially testing for n+1/p
-int npPowerfree(int str[], int sLen, int n, int p, int plus){
+int n_p_power_free(int str[], int sLen, int n, int p, int plus){
     while(n <= sLen){
         // for each letter in str
         for(int i = 0; i <= sLen - (n + plus); i++){
@@ -37,18 +37,30 @@ int npPowerfree(int str[], int sLen, int n, int p, int plus){
     return 1;
 }
 
-
-int main(){
-    int s[] = {2, 1, 2, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3};
-    // int s[] = {1, 2, 3, 4, 1, 2, 3, 4, 1, 2};
-    int sLen = 16;
-    int n = 2; 
-    int p = 1;
-    printf("is the string %d/%d-powerfree? %d\n", n, p, npPowerfree(s, sLen, n, p, 0));
-    printf("is the string %d/%d+-powerfree? %d\n", n, p, npPowerfree(s, sLen, n, p, 1));
+int * apply_bin_morph(int pre[], int preLen, int h0[], int h1[], int h0Len, int res[preLen*h0Len]){
+    int idxRes = 0;
+    for(int i = 0; i < preLen; i++){
+        for(int j = 0; j < h0Len; j++){
+            res[idxRes] = (pre[i] == 0)? h0[j] : h1[j];
+            idxRes++;
+        }
+    }
+    return idxRes;
 }
 
 
-// printf("i = %d\n", i);
-// printf("j = %d\n", j);
-// printf("step = %d\n", step);
+
+int main(){
+
+
+
+
+    // int s[] = {2, 1, 2, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3};
+    // // int s[] = {1, 2, 3, 4, 1, 2, 3, 4, 1, 2};
+    // int sLen = 16;
+    // int n = 2; 
+    // int p = 1;
+    // printf("is the string %d/%d-powerfree? %d\n", n, p, n_p_power_free(s, sLen, n, p, 0));
+    // printf("is the string %d/%d+-powerfree? %d\n", n, p, n_p_power_free(s, sLen, n, p, 1));
+}
+
