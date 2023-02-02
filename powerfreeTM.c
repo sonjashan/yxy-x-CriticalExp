fb49f53e041437c46187f93b0070f3b02101249b
sonjashan <102500611+sonjashan@users.noreply.github.com>
2023-02-02 11:59:20 -0500

    changed comments

--- powerfreeTM.c
+++ powerfreeTM.c
@@ -103,7 +103,7 @@ int fixed_len_avoid_yxyprimex(int str[], int sLen, int yLen, int xLen){
 }
 
 // str is binary
-// minimum |y| and |x|
+// xLen and yLen are the min length
 int avoid_yxyprimex(int str[], int sLen, int yLen, int xLen){
     for(int i = yLen; (i + xLen) * 2 <= sLen; i++){
         for(int j = xLen; (i + j) * 2 <= sLen; j++){
@@ -118,7 +118,7 @@ int avoid_yxyprimex(int str[], int sLen, int yLen, int xLen){
 
 // this is DFS!
 // for pre morphism sequences from 3 letter alphabets like vtm
-// xLen and yLen are the min length
+// xLen and yLen are the min length as in avoid_yxyprime()
 // mLen is the max length of morphisms we are looking for
 // return 0 not found, 1 found, -1 error
 int backtrack_search(int pre[], int preLen, int yLen, int xLen, int n, int p, int plus, int ltrMLen){
@@ -126,6 +126,8 @@ int backtrack_search(int pre[], int preLen, int yLen, int xLen, int n, int p, in
     int morphism[mLen];
     int i = 0; 
     morphism[i] = 0;
+
+    int count = 0;
     while(i < mLen){
         // printIntArray(morphism, i + 1, 0);
         int extend = 0;
@@ -178,6 +180,8 @@ int backtrack_search(int pre[], int preLen, int yLen, int xLen, int n, int p, in
                 morphism[i] = 1;
             }
         }
+        count++;
+        if(count % 10000 == 0) printf("checked %d potential sequences\n", count);
     }
     printf("ERROR: i should never >= mLen, as i only advance in if(extend)\n");
     return -1;
@@ -215,8 +219,8 @@ int main(){
 
     int yLen = 2;
     int xLen = 2;
-    int n = 3;
-    int p = 1;
+    int n = 5;
+    int p = 2;
     int plus = 0;
     int ltrMLen = 30;
 
