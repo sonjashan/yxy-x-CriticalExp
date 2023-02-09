f88355c68bab85a4e1edeb501edf66f38dd62713
sonjashan <102500611+sonjashan@users.noreply.github.com>
2023-02-08 22:16:03 -0500

    attempt to fix: morphisms have to be uniform we increase h(0) and such one letter at a time, so sometimes we dont have enough suffixes? better idea: a1b1c1a1'b1'c1' instead of a1a1'b1b1'c1c1'

--- powerfree.c
+++ powerfree.c
@@ -214,33 +214,53 @@ int backtrack_search(int pre[], int preLen, int yLen, int xLen, int n, int p, in
             backtrack = 0;
             extend = 1;
 
-            int h0Len = paLen + saLen;
-            int h0[h0Len]; 
-            concat(pa, paLen, sa, saLen, h0);
-            int h1Len = pbLen + sbLen;
-            int h1[h1Len]; 
-            concat(pb, pbLen, sb, sbLen, h1);
-            int h2Len = pcLen + scLen;
-            int h2[h2Len]; 
-            concat(pc, pcLen, sc, scLen, h2);
+            int skip = 0;
+            int h0Len;
+            int * h0;
+            int * h1;
+            int * h2;
+            if(paLen == pbLen && pbLen == pcLen){
+                if(saLen == sbLen && sbLen == scLen){
+                    h0Len = paLen + saLen;
+                    
+                    int ha[h0Len]; 
+                    concat(pa, paLen, sa, saLen, ha);
+                    h0 = &ha;
+                    
+                    int hb[h0Len]; 
+                    concat(pb, pbLen, sb, sbLen, hb);
+                    h1 = &hb;
+                    
+                    int hc[h0Len]; 
+                    concat(pc, pcLen, sc, scLen, hc);
+                    h2 = &hc;
+                }
+                h0Len = paLen;
+                h0 = &pa;
+                h1 = &pb;
+                h2 = &pc;
+            } else skip = 1;
 
-            int postMorph[max(h0Len, h1Len, h2Len) * preLen];
-            int postMorphLen = apply_tern_morph(pre, preLen, h0, h0Len, h1, h1Len, h2, h2Len, postMorph);
+            if(!skip){
+                int postMorphLen = h0Len * preLen;
+                int postMorph[postMorphLen];
+                apply_tern_morph(pre, preLen, h0, h0Len, h1, h0Len, h2, h0Len, postMorph);
 
-            if(avoid_yxyprimex(postMorph, postMorphLen, yLen, xLen) &&
-            n_p_powerfree(postMorph, postMorphLen, n, p, plus)){
+                if(avoid_yxyprimex(postMorph, postMorphLen, yLen, xLen) &&
+                n_p_powerfree(postMorph, postMorphLen, n, p, plus)){
 
-                printf("0->");
-                printIntArray(h0, h0Len, 0);
-                printf("1->");
-                printIntArray(h1, h1Len, 0);
-                printf("2->");
-                printIntArray(h2, h2Len, 0);
-                return 1;
+                    printf("0->");
+                    printIntArray(h0, h0Len, 0);
+                    printf("1->");
+                    printIntArray(h1, h0Len, 0);
+                    printf("2->");
+                    printIntArray(h2, h0Len, 0);
+                    return 1;
 
-            } else if(i >= mLen - 1){
-                extend = 0;
-                backtrack = 1;
+                } else if(i >= mLen - 1){
+                    extend = 0;
+                    backtrack = 1;
+                }
             }
         }        
         if(extend){
